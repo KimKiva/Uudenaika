ca5632b4091aa7d8baf53797e27ed66bb5d0135e
KimKiva <kim.kiva@hotmail.fi>
2025-04-28 15:33:36 +0300

    Ylä-navigaatiovalikon linkit ja animaatiot päivitetty

--- resources/views/layouts/partials/header-right-guest.blade.php
+++ resources/views/layouts/partials/header-right-guest.blade.php
@@ -1,11 +1,25 @@
 <!-- Vieraskäyttäjälle näkyvät linkit, kuten "Kirjaudu Sisään" ja "Liity" -->
 <ul class="hidden sm:flex gap-4">
-    <li><a class="text-sm text-gray-500 hover:text-blue-500" href="/login">Kirjaudu Sisään</a></li>
-    <li><a class="text-sm text-gray-500 hover:text-blue-500" href="/register">Liity</a></li>
+    <li>
+        <x-nav-link href="/login" :active="request()->is('login')" >
+            Kirjaudu Sisään
+        </x-nav-link>
+    </li>
+    <li>
+        <x-nav-link href="/register" :active="request()->is('register')" >
+            Liity
+        </x-nav-link>
+    </li>
 </ul>
 
-<!-- Mobiilissa näkyvät linkit -->
+
+<!-- Mobiilissa näkyvät linkit Jetstreamin x-nav-link -komponentilla ilman pallukoita -->
 <div class="sm:hidden flex flex-col items-center gap-2 mt-2">
-    <a class="text-sm text-gray-500 hover:text-blue-500" href="/login">Kirjaudu Sisään</a>
-    <a class="text-sm text-gray-500 hover:text-blue-500" href="/register">Liity</a>
+    <x-nav-link href="/login" :active="request()->is('login')" class="text-sm text-gray-500 hover:text-blue-500">
+        Kirjaudu Sisään
+    </x-nav-link>
+    <x-nav-link href="/register" :active="request()->is('register')" class="text-sm text-gray-500 hover:text-blue-500">
+        Liity
+    </x-nav-link>
 </div>
+
