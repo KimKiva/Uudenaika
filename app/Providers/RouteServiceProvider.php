1d55463aadbc7294eda8cfdaa87c3f21234bd683
KimKiva <kim.kiva@hotmail.fi>
2025-04-28 21:56:11 +0300

    Home-muuttuja muutettu dashboardista muotoon /

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Sovelluksen kotisivu käyttäjille kirjautumisen jälkeen.
+     */
+    public const HOME = '/'; // <-- Muutettu etusivulle
+
+    public function boot(): void
+    {
+        $this->routes(function () {
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::prefix('api')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+        });
+    }
+}
