647522555f8239edc00cf53a2acf23feaec00985
KimKiva <kim.kiva@hotmail.fi>
2025-05-03 23:50:11 +0300

    postauksien kommentointi lisätty

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -43,6 +43,10 @@ class Post extends Model
         return $this->belongsToMany(Category::class);
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 
     public function likes()
     {
